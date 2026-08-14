26ce12d53c455af3b0ddfd22412e9cc0e9d75b41
nox-security <info@nox-security.co.uk>
2026-08-14 15:05:05 +0100

    icon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,17 +20,6 @@ export const metadata: Metadata = {
   applicationName: site.name,
   category: "Fire and security systems",
   manifest: "/favicon/site.webmanifest",
-  icons: {
-    icon: [
-      { url: "/favicon/favicon.ico" },
-      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon/favicon-48x48.png", sizes: "48x48", type: "image/png" },
-      { url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-    ],
-    shortcut: "/favicon/favicon.ico",
-    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
-  },
   robots: isPreview
     ? { index: false, follow: false, nocache: true, googleBot: { index: false, follow: false, noimageindex: true } }
     : { index: true, follow: true, googleBot: { index: true, follow: true, "max-image-preview": "large", "max-snippet": -1, "max-video-preview": -1 } },
